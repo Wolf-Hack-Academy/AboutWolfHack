923725d5e0a7ca01b42c54a48405a35ef360c5f9
marmarkaroo <masayaf8@gmail.com>
2024-02-06 14:44:51 -0800

    added resources

--- src/views/ResourcesView.vue
+++ src/views/ResourcesView.vue
@@ -1,5 +1,21 @@
 <template>
     <div class="about">
       <h1>Related Resources</h1>
+      <h2>Problem Domain Book</h2>
+      <p>K. Daimi, G. Francia III. (2020). Innovations in Cybersecurity Education. ISBN: 978-3-030-50243-0.</p>
+      <p>This book discusses various techniques, methods, and approaches that cybersecurity experts use to educate others. It provides a detailed explanation of cybersecurity concepts with practical examples, as well as includes a roadmap of future trends for innovative cybersecurity education and training.</p>
+      <img src="./assets/978-3-030-50244-7.png">
+
+      <h2>pwn.college</h2>
+      <p>ASU’s pwn.college is useful for our development because it has a similar idea to our project as it is university run with different modules that a user can complete for points, which compete on a leaderboard.</p>
+      <h3>https://pwn.college/</h3>
+
+      <h2>National Cyber League</h2>
+      <p>NCL, or National Cyber League, is a cybersecurity competition that allows students to solve real-world challenges through the use of open source intelligence, forensics, cryptography, log analysis, and many more categories. This competition is restricted to students but the problems and topics will be similar to what Wolf Hack Academy has to offer, accessible to a wider audience.</p>
+      <h3>https://nationalcyberleague.org/competition</h3>
+
+      <h2>Game-based Learning Platform to Enhance Cybersecurity Education</h2>
+      <p>Khan, M.A., Merabet, A., Alkaabi, S. et al. Game-based learning platform to enhance cybersecurity education. Educ Inf Technol 27, 5153–5177 (2022). https://doi.org/10.1007/s10639-021-10807-6</p>
+      <p>      This article describes the use of the ARCS model of motivation in addition to a game-based learning platform to educate students in cybersecurity. This platform was developed for a university and targeted toward college students. Furthermore, this article describes the limitations of cyber competitions in teaching cybersecurity. All of these considerations are helpful for our team to determine how best to make a learning platform for cybersecurity targeted toward college students.</p>
     </div>
   </template>
